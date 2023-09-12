8b7956e10c5532499d7a5f59f00306c687777145
Leventbk <leventbkarakus@hotmail.com>
2023-09-12 12:59:22 -0400

    angryButton function completed

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -1,10 +1,21 @@
 import React, { useState } from 'react'; //optional
 
 function AngryButton() {
+  const [anger, setAnger] = useState(0);
+
+  const increaseAnger = () => {
+    if (anger < 1) {
+      setAnger(anger + 0.1);
+    }
+  }
+  const buttonStyle = {
+    backgroundColor: `rgba(255, 0, 0, ${anger})`
+  }
+
   return (
-    <button className="AngryButton">
-      {/* When the threshold is not reached */}<span>Don't click me too much! </span>
-      {/* When the threshold is not reached */}<span>Rawr!</span>
+    <button className="AngryButton" onClick={increaseAnger} style={buttonStyle}>
+      {anger < 1 && <span>Don't click me too much! </span>}
+      {anger > 1 && <span>Rawr!</span>}
     </button>
   )
 }
