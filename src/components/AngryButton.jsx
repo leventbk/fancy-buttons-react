39f96d4141c4e8f1ea050d82685c2cd83f6e8aed
Leventbk <leventbkarakus@hotmail.com>
2023-09-12 19:45:10 -0400

    refactoring: useState to useReducer

--- src/components/AngryButton.jsx
+++ src/components/AngryButton.jsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'; //optional
+import React, { useReducer } from 'react';
 
 function AngryButton() {
-  const [anger, setAnger] = useState(0);
-
-  const increaseAnger = () => {
+  const [anger, dispatch] = useReducer((anger, action) => {
     if (anger < 1) {
-      setAnger(anger + 0.1);
-    }
-  }
+      return anger + action;
+    } else { return 0};
+  }, 0)
+
   const buttonStyle = {
     backgroundColor: `rgba(255, 0, 0, ${anger})`
   }
 
   return (
-    <button className="AngryButton" onClick={increaseAnger} style={buttonStyle}>
+    <button className="AngryButton" onClick={() => dispatch(0.1)} style={buttonStyle}>
       {anger < 1 && <span>Don't click me too much! </span>}
       {anger > 1 && <span>Rawr!</span>}
     </button>
