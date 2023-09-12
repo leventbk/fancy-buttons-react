f68f1760776720a165609c0034445261619f85fb
Leventbk <leventbkarakus@hotmail.com>
2023-09-12 11:32:52 -0400

    component funcitons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import AngryButton from './components/AngryButton';
 
 function App() {
   return (
     <div className="App">
-
+      <AngryButton/>
     </div>
   );
 }
